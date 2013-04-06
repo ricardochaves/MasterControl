18b792714ffb1872da19a09fd07f1649c832d03e
Tiago Correa <tiagoc.mail@gmail.com>
2013-04-06 16:42:00 -0300

    Classes financeiro

---
+++ ServidorWeb/Financeiro/estoque.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Financeiro
+{
+    public class Produto
+    {
+        public string nome;
+        public double preco;
+        public tipoLoja[] loja;
+
+        public enum tipoLoja 
+        {
+            Mercadolivre = (int)1,
+            BomNegocio = (int)2,
+            OLX = (int)3,
+            TodOferta = (int)4,
+            Miguel = (int)5
+        }
+
+        public double consultaPrecoProduto(int codProduto, tipoLoja codLoja)
+        {            
+            using (NerdEntities N = new NerdEntities())
+            {
+                LojaProduto lp = new LojaProduto();                
+                
+                lp = N.LojaProduto.First(u => u.Cod_Produto == codProduto && u.Cod_Loja == (int)codLoja);
+
+                return (double)lp.Preco;                
+            }
+        }
+
+        public double consultaPrecoProdutoTarifado(int codProduto, tipoLoja codLoja)
+        {
+            using (NerdEntities N = new NerdEntities())
+            {
+                LojaProduto lp = new LojaProduto();
+
+                lp = N.LojaProduto.First(u => u.Cod_Produto * u.Tarifa == codProduto && u.Cod_Loja == (int)codLoja);
+
+                return (double)lp.Preco;
+            }
+        }        
+    }
+  }
---
+++ ServidorWeb/Financeiro/movimentacaoCaixa.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Financeiro
+{
+    public class movimentacaoCaixa
+    {
+        public tipoCaixa _caixa;
+        public double _valor;
+        public categoria _categoria;
+        public Dictionary<categoria, double> _split;
+
+        public movimentacaoCaixa()
+        {
+
+        }
+
+        public enum tipoCaixa
+        {
+            Tiago = (int)3,
+            Ricardo = (int)2,
+            NerdSa = (int)1
+        }
+
+        public enum categoria
+        {
+            Correios = (int)1,
+            Papelaria = (int)2,
+            Transporte = (int)3,
+            Prejuizo = (int)4,
+            VendaProduto = (int)5
+        }
+
+        public enum tipoAcao
+        {
+            deposito,
+            saque
+        }
+
+        public void deposito(tipoCaixa c, double v, categoria cc)
+        {
+            Caixa C = new Caixa();
+
+            using (NerdEntities N = new NerdEntities())
+            {
+                C.Cod_Caixa = (int)c;
+                C.Cod_Categ = (int)cc;
+                C.valor = (decimal)v;
+
+                N.Caixa.AddObject(C);
+                N.SaveChanges();
+            }
+        }
+
+        public void saque(tipoCaixa c, double v, categoria cc)
+        {
+
+            Caixa C = new Caixa();
+
+            using (NerdEntities N = new NerdEntities())
+            {
+                C.Cod_Caixa = (int)c;
+                C.Cod_Categ = (int)cc;
+                C.valor = -(decimal)v;
+
+                N.Caixa.AddObject(C);
+                N.SaveChanges();
+            }
+        }
+
+        public void deposito(tipoCaixa c, Dictionary<categoria, double> cc)
+        {
+            Caixa C = new Caixa();
+
+            using (NerdEntities N = new NerdEntities())
+                foreach (KeyValuePair<categoria, double> pair in cc)
+                {
+                    C.Cod_Caixa = (int)c;
+                    C.Cod_Categ = (decimal)pair.Key;
+                    C.valor = (decimal)pair.Value;
+
+                    N.Caixa.AddObject(C);
+                    N.SaveChanges();
+                }
+        }
+
+        public void saque(tipoCaixa c, Dictionary<categoria, double> cc)
+        {
+
+            Caixa C = new Caixa();
+
+            using (NerdEntities N = new NerdEntities())
+                foreach (KeyValuePair<categoria, double> pair in cc)
+                {
+                    C.Cod_Caixa = (int)c;
+                    _categoria = pair.Key;
+                    _valor = -(double)pair.Value;
+
+                    N.Caixa.AddObject(C);
+                    N.SaveChanges();
+                }
+        }
+
+    }
+}
