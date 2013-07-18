c42a21a283c61cc7a09bbdd77e03ce945f175438
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2013-07-18 09:51:22 -0300

    Update ConverterObjetoMLparaEF.cs

--- ServidorWeb/ML/Classes/ConverterObjetoMLparaEF.cs
+++ ServidorWeb/ML/Classes/ConverterObjetoMLparaEF.cs
@@ -375,63 +375,7 @@ namespace ServidorWeb.ML.Classes
 
                 ML_Shipping s = new ML_Shipping();
                 s = ConverteShipping(o, n);
-                ord.ML_Shipping.Add(s);
-
-                //if (o.shipping.id != null)
-                //{
-                //    //IMPLEMENTAR ENDERECO
-                //    s.cost = o.shipping.cost;
-                //    s.currency_id = o.shipping.currency_id;
-                //    if (o.shipping.date_created != null)
-                //    {
-                //        s.date_created = Convert.ToDateTime(o.shipping.date_created);
-                //    }
-                //    s.id = Convert.ToDecimal(o.shipping.id);
-                //    s.shipment_type = o.shipping.shipment_type;
-                //    s.status = o.shipping.status;
-
-                //     if (o.shipping.receiver_address != null && o.shipping.receiver_address.id != null)
-                //    {
-
-                //        ML_ReceiverAddress rc;
-                //        decimal d = Convert.ToDecimal(o.shipping.receiver_address.id);
-                //        rc = (from p in n.ML_ReceiverAddress where p.ID == d select p).FirstOrDefault();
-                //        if (rc == null)
-                //        {
-                //            rc = new ML_ReceiverAddress();
-                //            rc.address_line = o.shipping.receiver_address.address_line;
-                //            rc.comment = o.shipping.receiver_address.comment;
-                //            rc.ID = Convert.ToDecimal(o.shipping.receiver_address.id.ToString());
-                //            //rc.latitude = o.shipping.receiver_address.latitude.ToString(); Todos vem Null
-                //            //rc.longitude = o.shipping.receiver_address.longitude.ToString();
-                //            rc.zip_code = o.shipping.receiver_address.zip_code;
-
-
-                //            rc.ML_State = ControlEnd.RetonarStado(o.shipping.receiver_address.state.id, n);
-                //            if (rc.ML_State == null && o.shipping.receiver_address.state.id != null)
-                //            {
-                //                rc.ML_State = ConverteState(o.shipping.receiver_address.state);
-                //            }
-
-                //            rc.ML_City = ControlEnd.RetonarCidade(o.shipping.receiver_address.city.id, n);
-                //            if (rc.ML_City == null && o.shipping.receiver_address.city.id != null)
-                //            {
-                //                rc.ML_City = ConverteCity(o.shipping.receiver_address.city);
-                //            }
-
-                //            rc.ML_Country = ControlEnd.RetonarPais(o.shipping.receiver_address.country.id, n);
-                //            if (rc.ML_Country == null && o.shipping.receiver_address.country.id != null)
-                //            {
-                //                rc.ML_Country = ConverteCountry(o.shipping.receiver_address.country);
-                //            }
-                //        }
-
-                //        s.ML_ReceiverAddress = rc;
-                //    }
-
-                //    ord.ML_Shipping.Add(s);
-                //}
-
+                if (s != null) ord.ML_Shipping.Add(s);
 
                 //DADOS DA ORDEM
                 ord.id = o.id;
@@ -447,7 +391,7 @@ namespace ServidorWeb.ML.Classes
             }
             catch (Exception ex)
             {
-                throw new Exception(string.Format("Erro na rotina de ConverteOrdem2. OrdemID: {0}",o.id), ex);
+                throw new Exception(string.Format("Erro na rotina de ConverteOrdem. OrdemID: {0}",o.id), ex);
             }
         }
 
@@ -670,7 +614,7 @@ namespace ServidorWeb.ML.Classes
         }
         public ML_Shipping ConverteShipping(Order o, NSAADMEntities n)
         {
-            ML_Shipping ship = new ML_Shipping();
+            
             ControlaEndereco ControlEnd = new ControlaEndereco();
 
             if (o.shipping.id != null)
@@ -686,7 +630,7 @@ namespace ServidorWeb.ML.Classes
                 s.id = Convert.ToDecimal(o.shipping.id);
                 s.shipment_type = o.shipping.shipment_type;
                 s.status = o.shipping.status;
-
+                
                 if (o.shipping.receiver_address != null && o.shipping.receiver_address.id != null)
                 {
 
@@ -725,7 +669,7 @@ namespace ServidorWeb.ML.Classes
 
                     s.ML_ReceiverAddress = rc;
                 }
-                return ship;
+                return s;
             }
             else
             {
